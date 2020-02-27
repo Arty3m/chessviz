967ff8f155e0b30bce528d21512043a0c19d0303
artyom_kolbun <kolbunartema@gmail.com>
2020-02-27 23:19:02 +0700

    add function startboard

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+void startboard(char board[9][9])
+{
+    int i, j;
+    for (i = 0; i < 9; i++)
+        for (j = 0; j < 9; j++) {
+            board[i][j] = '\0';
+        }
+}
 
 int main()
 {
+    char board[9][9];
+    system("PAUSE");
     return 0;
 }
