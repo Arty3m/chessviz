cbc50a2ff587241a149c765227611181095ed14d
artyom_kolbun <kolbunartema@gmail.com>
2020-02-27 23:21:11 +0700

    add function printboard

--- main.c
+++ main.c
@@ -8,7 +8,15 @@ void startboard(char board[9][9])
             board[i][j] = '\0';
         }
 }
-
+void printboard(char board[9][9])
+{
+    int i, j;
+    for (i = 0; i < 9; i++) {
+        for (j = 0; j < 9; j++)
+            printf("%c ", board[i][j]);
+        printf("\n");
+    }
+}
 int main()
 {
     char board[9][9];
