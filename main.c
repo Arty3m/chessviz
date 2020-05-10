c760a4b69bf5867b18b99261144ee05c9e2ea91d
Artyom Kolbun <kolbunartema@gmail.com>
2020-05-10 20:30:32 +0700

    Added an algorithm for checking the move

--- main.c
+++ main.c
@@ -71,11 +71,53 @@ void printboard(char board[9][9], int n)
 }
 int main()
 {
-    int n = 9;
+    int i, j, n = 9, k = 3, p1 = 0, t = 0, f = 0;
     char hod[10], board[9][9], boardtemp[9][9];
     startboard(board, n);
     printboard(board, n);
 
+    while (1) {
+        if (k % 2 == 1) {
+            printf("White turn. Enter hod: ");
+
+            scanf("%s", hod);
+            if (hod[0] == 'a' || hod[0] == 'b' || hod[0] == 'c' || hod[0] == 'd'
+                || hod[0] == 'e' || hod[0] == 'f' || hod[0] == 'g'
+                || hod[0] == 'h') {
+                for (i = 0; i < 8; i++) {
+                    for (j = 1; j < 9; j++) {
+                        boardtemp[i][j] = board[i][j];
+                    }
+                }
+
+                for (i = 1; i < n; i++)
+                    if (hod[0] == board[8][i])
+                        t = i;
+                p1 = 8 - (hod[1] - 48);
+
+                for (i = 0; i < 8; i++) {
+                    for (j = 1; j < 9; j++) {
+                        if (boardtemp[i][j] != board[i][j]) {
+                            f++;
+                        }
+                    }
+                }
+                if (f > 0)
+                    k++;
+                for (i = 0; i < n; i++)
+                    hod[i] = '\0';
+                if (f == 0) {
+                    printf("Incorrect hod\n");
+                    printboard(board, n);
+                }
+                f = 0;
+
+            } else {
+                printf("Incorrect hod\n");
+                printboard(board, n);
+            }
+        }
+    } // while end
     system("PAUSE");
     return 0;
 }
