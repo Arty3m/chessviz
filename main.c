1b5c40f5f692e2d4932e7382075aa585525d8d19
Artyom Kolbun <kolbunartema@gmail.com>
2020-05-10 20:46:32 +0700

    Added an algorithm for checking the black move

--- main.c
+++ main.c
@@ -103,7 +103,6 @@ int main()
     char hod[10], board[9][9], boardtemp[9][9];
     startboard(board, n);
     printboard(board, n);
-<<<<<<< HEAD
 
     while (1) {
         if (k % 2 == 1) {
@@ -151,14 +150,52 @@ int main()
                 printboard(board, n);
             }
         }
-    } // while end
-||||||| merged common ancestors
+        if (k % 2 == 0) {
+            printf("Black turn. Enter hod: ");
 
-=======
-    scanf("%s", hod);
-    WhPesh(board, n, hod);
-    printboard(board, n);
->>>>>>> MoveWhitePawn
+            scanf("%s", hod);
+            if (hod[0] == 'a' || hod[0] == 'b' || hod[0] == 'c' || hod[0] == 'd'
+                || hod[0] == 'e' || hod[0] == 'f' || hod[0] == 'g'
+                || hod[0] == 'h') {
+                for (i = 0; i < 8; i++) {
+                    for (j = 1; j < 9; j++) {
+                        boardtemp[i][j] = board[i][j];
+                    }
+                }
+                for (i = 1; i < n; i++)
+                    if (hod[0] == board[8][i])
+                        t = i;
+
+                p1 = 8 - (hod[1] - 48);
+
+                if (board[p1][t] == 'p') {
+                    BlPesh(board, n, hod);
+                    printboard(board, n);
+                }
+
+                for (i = 0; i < 8; i++) {
+                    for (j = 1; j < 9; j++) {
+                        if (boardtemp[i][j] != board[i][j]) {
+                            f++;
+                        }
+                    }
+                }
+                if (f > 0)
+                    k++;
+                for (i = 0; i < n; i++)
+                    hod[i] = '\0';
+                if (f == 0) {
+                    printf("Incorrect hod\n");
+                    printboard(board, n);
+                }
+                f = 0;
+
+            } else {
+                printf("Incorrect hod\n");
+                printboard(board, n);
+            }
+        }
+    } // while end
     system("PAUSE");
     return 0;
 }
