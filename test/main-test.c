1ec2833380c64ea713e6c7ae85bb37e4e6cb25af
Artyom Kolbun <kolbunartema@gmail.com>
2020-05-16 00:21:16 +0700

    Add test for startboard

--- test/main-test.c
+++ test/main-test.c
@@ -96,6 +96,30 @@ CTEST(Queue, HaveDiff)
     int result = Queue(3, board, boardtemp, hod, n);
     ASSERT_EQUAL(4, result);
 }
+CTEST(startboard, AccuracyofFilling)
+{
+    int result;
+    startboard(board, n);
+
+    if (board[0][0] == '8' && board[0][1] == 'r' && board[0][2] == 'n'
+        && board[0][3] == 'b' && board[0][4] == 'q' && board[0][5] == 'k'
+        && board[0][6] == 'b' && board[0][7] == 'n' && board[0][8] == 'r'
+        && board[1][0] == '7' && board[1][1] == 'p' && board[1][2] == 'p'
+        && board[1][3] == 'p' && board[1][4] == 'p' && board[1][5] == 'p'
+        && board[1][6] == 'p' && board[1][7] == 'p' && board[1][8] == 'p'
+        && board[2][0] == '6' && board[3][0] == '5' && board[4][0] == '4'
+        && board[5][0] == '3' && board[6][0] == '2' && board[6][1] == 'P'
+        && board[6][2] == 'P' && board[6][3] == 'P' && board[6][4] == 'P'
+        && board[6][5] == 'P' && board[6][6] == 'P' && board[6][7] == 'P'
+        && board[6][8] == 'P' && board[7][0] == '1' && board[7][1] == 'R'
+        && board[7][2] == 'N' && board[7][3] == 'B' && board[7][4] == 'Q'
+        && board[7][5] == 'K' && board[7][6] == 'B' && board[7][7] == 'N'
+        && board[7][8] == 'R' && board[8][1] == 'a' && board[8][2] == 'b'
+        && board[8][3] == 'c' && board[8][4] == 'd' && board[8][5] == 'e'
+        && board[8][6] == 'f' && board[8][7] == 'g' && board[8][8] == 'h')
+        result = 0;
+    ASSERT_EQUAL(0, result);
+}
 int main(int argc, const char** argv)
 {
     return ctest_main(argc, argv);
